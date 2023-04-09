6366f3aca273c07e40ef3287fff110ca79449b90
Sergey <darkus007@yandex.ru>
2023-04-09 17:58:42 +0300

    Установлены sqlalchemy и alembic. Написаны модели Project, Flat и Price декларативным методом, создана миграция.

--- src/main.py
+++ src/main.py
@@ -1,11 +1,20 @@
 from flask import Flask
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+
+from models import engine, Price, Flat, Project
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    with Session(engine) as session:
+        stmt = select(Flat, Price).join(Price).limit(10)
+        for res in session.execute(stmt):
+            print(f"{res.Price.price=}")
+
+    return f"<p>Hello, World!</p>"
 
 
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -1,50 +1,65 @@
-from sqlalchemy import MetaData, Table, Column, ForeignKey
-from sqlalchemy import Integer, String, Float, Date, Boolean
+from datetime import datetime
+from typing import Optional
 
-metadata = MetaData()
+from sqlalchemy import ForeignKey, Integer, String, Float, Date, Boolean, create_engine
+from sqlalchemy.orm import declarative_base, Mapped, mapped_column, relationship
 
-projects = Table(
-    "Projects",
-    metadata,
-    Column('project_id', Integer, primary_key=True),
-    Column('city', String(127), nullable=False),
-    Column('name', String(127), nullable=False),
-    Column('url', String(255)),
-    Column('metro', String(127)),
-    Column('time_to_metro', Integer),
-    Column('latitude', Float),
-    Column('longitude', Float),
-    Column('address', String(255)),
-    Column('data_created', Date),
-    Column('data_closed', Date),
-)
+from config import DB_USER, DB_PASS, DB_NAME, DB_PORT, DB_HOST
 
-flats = Table(
-    "Flats",
-    metadata,
-    Column('flat_id', Integer, primary_key=True),
-    Column('project_id', Integer, ForeignKey('Projects.project_id')),
-    Column('address', String(127), nullable=False),
-    Column('floor', Integer),
-    Column('rooms', Integer),
-    Column('area', Float),
-    Column('finishing', Boolean),
-    Column('bulk', String(127)),
-    Column('settlement_date', Date),
-    Column('url_suffix', String(127)),
-    Column('data_created', Date),
-    Column('data_closed', Date),
-)
+engine = create_engine(f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}", echo=True)
 
 
-prices = Table(
-    "Prices",
-    metadata,
-    Column('flat_id', Integer, ForeignKey('Flats.flat_id')),
-    Column('benefit_name', String(127)),
-    Column('benefit_description', String(255)),
-    Column('price', Integer),
-    Column('meter_price', Integer),
-    Column('booking_status', String(15)),
-    Column('data_created', Date),
-)
+Base = declarative_base()
+
+
+class Project(Base):
+    __tablename__ = "project"
+
+    project_id: Mapped[int] = mapped_column(primary_key=True)
+    city: Mapped[str] = mapped_column(String(127))
+    name: Mapped[str] = mapped_column(String(127))
+    url: Mapped[str | None] = mapped_column(String(255))
+    metro: Mapped[str | None] = mapped_column(String(127))
+    time_to_metro: Mapped[int | None] = mapped_column(Integer)
+    latitude: Mapped[float | None] = mapped_column(Float)
+    longitude: Mapped[float | None] = mapped_column(Float)
+    address: Mapped[str | None] = mapped_column(String(255))
+    data_created: Mapped[datetime.date] = mapped_column(Date)
+    data_closed: Mapped[Optional[datetime.date]] = mapped_column(Date)
+
+    flat: Mapped[list["Flat"]] = relationship(back_populates="project")
+
+
+class Flat(Base):
+    __tablename__ = "flat"
+
+    flat_id: Mapped[int] = mapped_column(primary_key=True)
+    project_id: Mapped[int] = mapped_column(ForeignKey("project.project_id"))
+    address: Mapped[str | None] = mapped_column(String(255))
+    floor: Mapped[int | None] = mapped_column(Integer)
+    rooms: Mapped[int | None] = mapped_column(Integer)
+    area: Mapped[float | None] = mapped_column(Integer)
+    finishing: Mapped[bool | None] = mapped_column(Boolean)
+    bulk: Mapped[str | None] = mapped_column(String(127))
+    settlement_date: Mapped[Optional[datetime.date]] = mapped_column(Date)
+    url_suffix: Mapped[str] = mapped_column(String(127))
+    data_created: Mapped[datetime.date] = mapped_column(Date)
+    data_closed: Mapped[Optional[datetime.date]] = mapped_column(Date)
+
+    project: Mapped["Project"] = relationship(back_populates="flat")
+    prices: Mapped[list["Price"]] = relationship(back_populates="flat")
+
+
+class Price(Base):
+    __tablename__ = "price"
+
+    price_id: Mapped[int] = mapped_column(primary_key=True)
+    flat_id: Mapped[int] = mapped_column(ForeignKey("flat.flat_id"))
+    benefit_name: Mapped[str | None] = mapped_column(String(127))
+    benefit_description: Mapped[str | None] = mapped_column(String(255))
+    price: Mapped[int] = mapped_column(Integer)
+    meter_price: Mapped[int | None] = mapped_column(Integer)
+    booking_status: Mapped[str | None] = mapped_column(String(15))
+    data_created: Mapped[datetime.date] = mapped_column(Date)
+
+    flat: Mapped["Flat"] = relationship(back_populates="prices")
